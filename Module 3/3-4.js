0f2db51d227fd7db411c097948b726b928f7f2a4
Samiul Islam <samiul10203040@gmail.com>
2025-11-12 23:54:40 +0600

    queue implementation using array

---
+++ Module 3/3-4.js
@@ -0,0 +1,43 @@
+// Queue implementation using aray...
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(value) {
+    this.items.push(value);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.shift();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+console.log(queue.peek());
+
+queue.dequeue();
+queue.print();
