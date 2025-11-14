48c7c62e651253a6c5d9f6c82e571f20b8d7cbf3
Samiul Islam <samiul10203040@gmail.com>
2025-11-14 22:20:26 +0600

    non primitive types

---
+++ Module 5/Typescript learning/src/nonPrimitive.ts
@@ -0,0 +1,21 @@
+// array , object
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+//bazarList.push(true); // error
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 45];
+
+mixedArr.push(67);
+mixedArr.push("apple");
+mixedArr.push("sugar");
+
+// tuple - Ts type
+
+let coOrdinates: [number, number] = [20, 30];
+
+let couple: [string, string] = ["Husband", "Wife"];
+
+let samiulNameAndRoll: [string, number] = ["samiul", 21];
+
+let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
