06a47a21de97a14c8fd6e4aeab95d66161e404ee
Samiul Islam <samiul10203040@gmail.com>
2025-11-14 21:30:18 +0600

    exploring primitive types

---
+++ Module 5/Typescript learning/src/primitive.ts
@@ -0,0 +1,12 @@
+// Js - Ts
+// String , number boolean , undefined, null
+
+//  TS : never, unknown ,void, any
+
+let userName: string = "samiul123";
+
+let userId = 123;
+
+let isAdmin = true; // implicit type declaration
+
+let isUser: boolean = false; // explicit type declaration
